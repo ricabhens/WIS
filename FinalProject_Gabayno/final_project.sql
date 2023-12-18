01ba9310600d4037cc1075c3cb63d3bbcd41bdaf
ricabhens <ricabhensg@gmail.com>
2023-12-18 23:17:18 +0800

    FinalProject_Gabayno

--- FinalProject_Gabayno/final_project.sql
+++ FinalProject_Gabayno/final_project.sql
@@ -35,19 +35,3 @@ create table Enrollment(
     EnrollmentDate DATE,
     Grade int NOT NULL
 );
-
-insert into Student(StudentID, Firstname, Lastname, DateOfBirth, Email, Phone)
-values(135548, "Kamisato", "Ayato", 11/26/2002, "ayato@gmail.com", 09506549689);
-
-UPDATE student SET DateOfBirth = "2002-11-26" WHERE StudentID = 135548;
-
-insert into Course (CourseID, CourseName, Credits)
-values(1, "CIT17", "3.0");
-
-insert into Instructor(InstructorID, Firstname, Lastname, Email, Phone)
-values(546215, "Quan", "Zhongli", "zhongli@gmail.com", 09212437823);
-
-insert into Enrollment(EnrollmentID, StudentID, CourseID, EnrollmentDate, Grade)
-values(13, 135548, 1, 2023-08-16, 90);
-
-UPDATE enrollment SET EnrollmentDate = "2023-08-16" WHERE EnrollmentID = 13;
